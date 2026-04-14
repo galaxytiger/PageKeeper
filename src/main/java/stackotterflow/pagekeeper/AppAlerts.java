0f3e66359964b69140c62709f001719318d56912
ClaudioBardales <claudioeduardobardales@gmail.com>
2026-04-13 20:53:14 -0700

    Added showError for AppAlerts

--- src/main/java/stackotterflow/pagekeeper/AppAlerts.java
+++ src/main/java/stackotterflow/pagekeeper/AppAlerts.java
@@ -10,4 +10,12 @@ public class AppAlerts {
         alert.setContentText(message);
         alert.showAndWait();
     }
+
+    public static void showError(String title, String message){
+        Alert alert = new Alert(Alert.AlertType.ERROR);
+        alert.setTitle(title);
+        alert.setHeaderText(null);
+        alert.setContentText(message);
+        alert.showAndWait();
+    }
 }
