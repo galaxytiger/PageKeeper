0214006433a766f4ca22fc36bd1cf867e1b38ee6
ClaudioBardales <claudioeduardobardales@gmail.com>
2026-04-13 20:54:08 -0700

    Added showSuccess for AppAlerts

--- src/main/java/stackotterflow/pagekeeper/AppAlerts.java
+++ src/main/java/stackotterflow/pagekeeper/AppAlerts.java
@@ -18,4 +18,12 @@ public class AppAlerts {
         alert.setContentText(message);
         alert.showAndWait();
     }
+
+    public static void showSuccess(String message) {
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle("Success");
+        alert.setHeaderText(null);
+        alert.setContentText(message);
+        alert.showAndWait();
+    }
 }
